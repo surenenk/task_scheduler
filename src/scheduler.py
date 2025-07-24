050ef9f820beabaf7431e7d12c5d062635fdb0b4
Suren Eda <suren.eda@gmail.com>
2025-07-24 22:13:52 +0000

    Add task and dependency validation

--- src/scheduler.py
+++ src/scheduler.py
@@ -54,7 +54,50 @@ def get_tasks(file):
 
     return tasks
 
+# Check if there is cyclic dependency here. Use a stack and a visited set to keep track of
+# task and visited deps. If more than one task in stack with same name, there is a cycle.
+def is_dep_cyclic(tasks):
+    visited = set()
+    stack = set()
+
+    def dfs(task_name):
+        if task_name in stack:
+            return True
+
+        if task_name in visited:
+            return False
+
+        stack.add(task_name)
+
+        for dep in tasks[task_name].deps:
+            if dfs(dep):
+                return True
+
+        stack.remove(task_name)
+        visited.add(task_name)
+
+        return False
+
+    for task_name in tasks:
+        if dfs(task_name):
+            return True
+
+    return False
+
+# Validate the tasks. Check if they are valid like does the dep task exist in list of tasks,
+# does the task list create cyclical dependency. If so error out.
+def validate_tasks(tasks):
+    task_names = set(tasks.keys())
+
+    for task in tasks.values():
+        for dep in task.deps:
+            if dep not in task_names:
+                raise ValueError(f"Invalid dependency '{dep}' in task '{task.name}'")
+
+    if is_dep_cyclic(tasks):
+        raise ValueError("Cycle detected in task dependencies")
+
 if __name__ := "__main__":
     inputs = input_parser()
     tasks = get_tasks(inputs.file)
-    print(f"{inputs} and {tasks.keys()}")
+    validate_tasks(tasks)
